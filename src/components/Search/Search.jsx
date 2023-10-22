2a593f725f51cacbcabc841b917b5d251cebb1c3
conrado julio <conradojuliosisnero@gmail.com>
2023-10-21 21:19:35 -0500

    comentando el codigo

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,23 +2,33 @@ import { useState } from "react";
 import "./search.css";
 
 function Search({ onSearch }) {
+	// Estado local para almacenar el valor de la ciudad de búsqueda.
 	const [searchCity, setSearchCity] = useState("");
 
+	// Función que se ejecuta al enviar el formulario.
 	const onSubmit = (e) => {
-		e.preventDefault();
+		e.preventDefault(); // Evita la recarga de la página al enviar el formulario.
+
+		// Limpia y normaliza el nombre de la ciudad ingresada.
 		const cleanedCity = cleanAndNormalizeCityName(searchCity);
+
+		// Llama a la función "onSearch" con el nombre de la ciudad limpio.
 		onSearch(cleanedCity);
+
+		// Imprime el nombre de la ciudad limpio en la consola.
 		console.log(cleanedCity);
 	};
 
+	// Limpia y normaliza el nombre de la ciudad.
 	const cleanAndNormalizeCityName = (cityName) => {
-		// Elimina caracteres especiales y convierte a minúsculas
+		// Elimina caracteres especiales y convierte a minúsculas.
 		return cityName.replace(/[^a-zA-Z ]/g, "").toLowerCase();
 	};
 
 	return (
 		<div className="search">
 			<form onSubmit={onSubmit}>
+				{/* Campo de entrada de texto para la búsqueda. */}
 				<input
 					placeholder="Search..."
 					type="text"
@@ -26,6 +36,7 @@ function Search({ onSearch }) {
 					value={searchCity}
 					onChange={(e) => setSearchCity(e.target.value)}
 				/>
+				{/* Botón para iniciar la búsqueda. */}
 				<button type="submit">Search</button>
 			</form>
 		</div>
